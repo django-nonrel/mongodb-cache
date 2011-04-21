cef2f774456d32fccb1908077f53bb851b22e69e
Jonas Haag <jonas@lophus.org>
2011-04-21 23:27:54 +0200

    v0.2: Requires Django MongoDB Engine from Git repo (actually 0.4 which is not released yet)

--- setup.py
+++ setup.py
@@ -13,16 +13,16 @@ CLASSIFIERS = [
 
 setup(
     name='django_mongodb_cache',
-    version='0.1',
+    version='0.2',
     author='Jonas Haag',
     author_email='jonas@lophus.org',
-    url='http://github.com/django-mongodb-engine/mongodb-cache',
+    url='https://github.com/django-mongodb-engine/mongodb-cache',
     license='2-clause BSD',
     description="A Django MongoDB cache backend",
     long_description=None,
 
     platforms=['any'],
-    install_requires=['django_mongodb_engine>=0.2'],
+    install_requires=['django_mongodb_engine>=0.4'],
 
     packages=['django_mongodb_cache'],
     include_package_data=True,
